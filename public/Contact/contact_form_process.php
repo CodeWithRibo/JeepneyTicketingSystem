50cf756ee61bf56c4ece77902c1b8ea65b510eb3
CodeWithRibo <riboluna2818@gmail.com>
2025-02-02 18:28:43 +0800

    adding modal after success submit the form

--- public/Contact/contact_form_process.php
+++ public/Contact/contact_form_process.php
@@ -4,52 +4,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <title>Contact Process</title>
+    <title>Document</title>
 </head>
 <body>
-    
 <?php 
 include '../Database/dbconfig.php';
-
 if($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    $firstName = $_POST['firstName'];
-    $lastName = $_POST['lastName'];
-    $email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
-
-    if(empty($firstName) || empty($lastName) || empty($email) || empty($subject) || empty($message)){
-       ?> 
-        <script>
+     
+    include '../handler/contact_logic_handler.php';
+    ob_start();
+   // Validation 
+    if(!array_filter($contactErrors))  {
+         // if no errors proceed to the db
+         $sqlContact = "INSERT INTO contact (firstName, lastName, email, subject, message) VALUES (?,?,?,?,?)";
+         $insertContact = $connection -> prepare($sqlContact);
+         $insertContact -> bind_param("sssss", $firstName,$lastName, $email,$subject,$message);
+     
+         if($insertContact -> execute()) {
+       
+            ?> <script> 
             Swal.fire({
-                title: "Failed Submit",
-                text: "Please fill in all fields",
-                icon: "info"
-            }).then((result) => {
-                if(result.isConfirmed) {
-                    window.location = 'contact.php';
-                }
-            });
-        </script>
-       <?php
+                position: "top-end",
+                icon: "success",
+                title: "Your message have been submit",
+                showConfirmButton: false,
+                timer: 1500
+                }).then((result) => {
+                    if (result.dismiss === Swal.DismissReason.timer) {
+                        window.location = '../include/home_page.php';
+                    }
+                });
+            </script> <?php
+             exit();
+         } 
+         $insertContact -> close();
+         $connection -> close();
     }
-    // Validation
-    // include '../handler/contact_logic_handler.php';
-    
-    // if no errors proceed to the db
-    $sqlContact = "INSERT INTO contact (firstName, lastName, email, subject, message) VALUES (?,?,?,?,?)";
-    $insertContact = $connection -> prepare($sqlContact);
-    $insertContact -> bind_param("sssss", $firstName,$lastName, $email,$subject,$message);
-
-    if($insertContact -> execute()) {
-        header('Location: ../include/home_page.php');
-        exit();
-    } 
-    $insertContact -> close();
-    $connection -> close();
+    ob_end_flush();
 }
-?>
 
+?>
 </body>
 </html>
+
