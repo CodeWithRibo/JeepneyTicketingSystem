89d681d350bcc3636ec3e0927130080848b1ff39
CodeWithRibo <riboluna2818@gmail.com>
2025-02-04 21:24:44 +0800

    Adding buy ticket label (Login)

--- public/components/header.php
+++ public/components/header.php
@@ -31,12 +31,13 @@ $isLogin = isset($_SESSION["user_id"]);
 <body>
 <?php
 if (!$isLogin) {
+    // Logout
   echo '<header>
     <!--DESKTOP DESIGN -->
     <div class="static left-0 right-0 top-0 z-10 flex h-20 items-center justify-between bg-white shadow-lg sm:px-5 md:fixed md:px-10 lg:px-20">
         <div class="flex w-full items-center justify-center md:w-auto">
             <span class="mx-2">
-                <img class="h-[70px] w-[230px] object-cover px-5" src="/jts/image/main_logo.png" alt="JTS logo" />
+                <a href="../Logout/home_page.php"><img class="h-[70px] w-[230px] object-cover cursor-pointer px-5" src="/jts/image/main_logo.png" alt="JTS logo" /></a>
             </span>
             <!-- MOBILE DESIGN HEADER ICON -->
             <span class="pr-2 font-bold text-primary md:hidden">Menu</span>
@@ -48,10 +49,10 @@ if (!$isLogin) {
         </div>
         <div class="hidden md:block">
             <span class="flex gap-10 transition-all">
-                <a class="text-primary duration-300 ease-in hover:text-[#e6ce20] md:text-sm lg:text-xl" href="#home">Home</a>
-                <a class="text-primary duration-300 ease-in hover:text-[#e6ce20] md:text-sm lg:text-xl" href="BuyTicket.php">Buy Ticket</a>
-                <a class="text-primary duration-300 ease-in hover:text-[#e6ce20] md:text-sm lg:text-xl" href="#about">About</a>
-                <a class="text-primary duration-300 ease-in hover:text-[#e6ce20] md:text-sm lg:text-xl" href="#contact">Contact Us</a>
+                <a class="text-primary duration-300 ease-in hover:text-[#e6ce20] md:text-sm lg:text-xl" href="../logout/home_page.php">Home</a>
+                <a class="text-primary duration-300 ease-in hover:text-[#e6ce20] md:text-sm lg:text-xl" href="../include/buy_ticket.php">Buy Ticket</a>
+                <a class="text-primary duration-300 ease-in hover:text-[#e6ce20] md:text-sm lg:text-xl" href="../logout/home_page.php #about">About</a>
+                <a class="text-primary duration-300 ease-in hover:text-[#e6ce20] md:text-sm lg:text-xl" href="../logout/home_page.php #contact">Contact Us</a>
                 <a class="text-primary duration-300 ease-in hover:text-[#e6ce20] md:text-sm lg:text-xl" href="../Login/login.php">Login</a>
             </span>
         </div>
@@ -59,13 +60,14 @@ if (!$isLogin) {
     <!-- MOBILE DESIGN HEADER -->
     <div class="grid hidden grid-rows-5 justify-center gap-6 border-2 bg-white text-center shadow-lg transition-all md:hidden" id="mobile-menu">
         <a class="pt-3 text-primary duration-300 ease-in hover:text-[#e6ce20] md:text-sm md:font-semibold lg:text-xl" href="#home">Home</a>
-        <a class="text-primary duration-300 ease-in hover:text-[#e6ce20] md:text-sm md:font-semibold lg:text-xl" href="BuyTicket.php">Buy Ticket</a>
-        <a class="text-primary duration-300 ease-in hover:text-[#e6ce20] md:text-sm md:font-semibold lg:text-xl" href="#about">About</a>
-        <a class="text-primary duration-300 ease-in hover:text-[#e6ce20] md:text-sm md:font-semibold lg:text-xl" href="#contact">Contact Us</a>
-        <a class="pb-3 text-primary duration-300 ease-in hover:text-[#e6ce20] md:text-sm md:font-semibold lg:text-xl" href="Login.php">Login</a>
+        <a class="text-primary duration-300 ease-in hover:text-[#e6ce20] md:text-sm md:font-semibold lg:text-xl" href="../include/buy_ticket.php">Buy Ticket</a>
+        <a class="text-primary duration-300 ease-in hover:text-[#e6ce20] md:text-sm md:font-semibold lg:text-xl" href="../logout/home_page.php #about">About</a>
+        <a class="text-primary duration-300 ease-in hover:text-[#e6ce20] md:text-sm md:font-semibold lg:text-xl" href="../logout/home_page.php #contact">Contact Us</a>
+        <a class="pb-3 text-primary duration-300 ease-in hover:text-[#e6ce20] md:text-sm md:font-semibold lg:text-xl" href="../login/login.php">Login</a>
     </div>
 </header>';
 } else {
+    // Login
 echo '<header>
     <!--DESKTOP DESIGN -->
     <div class="static left-0 right-0 top-0 z-10 flex h-20 items-center justify-between bg-white shadow-lg sm:px-5 md:fixed md:px-10 lg:px-20">
@@ -80,9 +82,10 @@ echo '<header>
                 <div class="hidden md:block px-5">
                     <span class="flex gap-10 transition-all">
                         <span class="text-primary duration-300 ease-in hover:text-[#e6ce20] md:text-sm lg:text-xl"><a href="../include/home_page.php">Home</a></span>
+                        <span class="text-primary duration-300 ease-in hover:text-[#e6ce20] md:text-sm lg:text-xl"><a href="../include/buy_ticket.php">Buy Ticket</a></span>
                         <span class="text-primary duration-300 ease-in hover:text-[#e6ce20] md:text-sm lg:text-xl"><a href="#">Transaction History</a></span>
-                        <span class="text-primary duration-300 ease-in hover:text-[#e6ce20] md:text-sm lg:text-xl"><a href="#">About</a></span>
-                        <span class="text-primary duration-300 ease-in hover:text-[#e6ce20] md:text-sm lg:text-xl"><a href="#">Contact Us</a></span>
+                        <span class="text-primary duration-300 ease-in hover:text-[#e6ce20] md:text-sm lg:text-xl"><a href="../include/home_page.php #about">About</a></span>
+                        <span class="text-primary duration-300 ease-in hover:text-[#e6ce20] md:text-sm lg:text-xl"><a href="../include/home_page.php #contact">Contact Us</a></span>
                     </span>
                 </div>
                 <div class="dropdown dropdown-end flex items-center justify-center">
