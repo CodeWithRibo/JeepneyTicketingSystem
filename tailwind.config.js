1d0575f6a045ff56d11e877368f6308a47f8bbd1
CodeWithRibo <riboluna2818@gmail.com>
2025-02-07 18:11:05 +0800

    fixing tailwind v3 to v4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+ /** @type {import('tailwindcss').Config} */
+ export default {
   content: [
     "./public/**/*.{html,js,css,php}",
-    "./src/**/*.{html,js,css,php}",
-    "./*.{html,js,css,php}",
-    "./custom.css",
-    "./src/custom.css",
-],
+      "./src/**/*.{html,js,css,php}",
+      "./*.{html,js,css,php}",
+      "./custom.css",
+      "./src/custom.css",
+
+  ],
   theme: {
     extend: {
       colors: {
@@ -14,27 +15,11 @@ module.exports = {
         button: "#d5c812",
         background: "#e1e2e136",
         textColor: "#565656",
-        // daiysui colors
-        customYellow: "#e7f20f",
-            },
-            fontFamily: {
-        Ubuntu: ["Ubuntu"],
-        sanscalp: ["SansCalps", "sans-serif"],
-            },
-            gridTemplateColumns: {},
-          },
-        },
-        plugins: [
-          require('daisyui'),
-        ],
-        daisyui: {
-          themes: false, // Disable all themes
-          darkTheme: "dark", // Optional: name of one of the included themes for dark mode
-          base: false, // Disable background and foreground color for root element
-          styled: true, // Include Daisy UI colors and design decisions for all components
-          utils: true, // Adds responsive and modifier utility classes
-          prefix: "", // Prefix for Daisy UI classnames (optional)
-          logs: true, // Shows info about Daisy UI version and used config in the console when building your CSS
-          themeRoot: ":root", // The element that receives theme color CSS variables
+      },
+      fontFamily: {
+        sanscalp: ["SansCalps", "sans-serif"]
+      }
+    },
   },
+  plugins: [],
 }
