0f774895dc01cb21544960f2b6317c272ee9b670
CodeWithRibo <riboluna2818@gmail.com>
2025-02-07 18:04:04 +0800

    hello

--- public/components/footer.php
+++ public/components/footer.php
@@ -1,4 +1,9 @@
-<!DOCTYPE html>
+<?php
+//check the user if exist
+$isLogin = isset($_SESSION['user_id']);
+
+?>
+ <!DOCTYPE html>
  <html lang="en">
  <head>
     <meta charset="UTF-8">
@@ -25,16 +30,18 @@
       }
     </style>
  </head>
- <body>
+<?php
+ if (!$isLogin) {
+  echo '<body>
    <section class="bg-[#e9e5e5]">
       <main class="">
         <div class="main_container mx-auto min-h-full items-start justify-evenly py-20 text-center md:grid md:grid-cols-3 md:gap-10 md:px-20 md:text-start xl:flex xl:gap-44">
           <div class="m-0 mb-5 flex w-full flex-col gap-2 md:col-start-1 md:m-0 md:gap-5">
             <h1 class="animation_wrap font-semibold text-primary md:text-xl lg:text-2xl xl:text-[26px] 2xl:text-3xl">QUICK LINKS</h1>
-            <span class="text_wrap">Home</span>
-            <span class="text_wrap">Terminals</span>
-            <span class="text_wrap">Buy Ticket</span>
-            <span class="text_wrap">About</span>
+            <a href="#home"><span class="text_wrap">Home</span></a>
+            <a href="../include/buy_ticket.php #terminals"><span class="text_wrap">Terminals</span></a>
+            <a href="../include/buy_ticket.php"><span class="text_wrap">Buy Ticket</span></a>
+            <a href="../logout/home_page.php #about"><span class="text_wrap">About</span></a>
           </div>
           <div class="m-0 mb-5 flex w-full flex-col gap-2 md:col-start-1 md:row-start-2 md:m-0 md:gap-5">
             <h1 class="animation_wrap font-semibold text-primary md:text-xl lg:text-2xl xl:text-[26px] 2xl:text-3xl">CONTACT INFO</h1>
@@ -45,9 +52,8 @@
           </div>
           <div class="m-0 flex w-full flex-col gap-2 md:col-start-2 md:row-start-1 md:m-0 md:ml-10 md:gap-5">
             <h1 class="animation_wrap font-semibold text-primary md:text-xl lg:text-2xl xl:text-[26px] 2xl:text-3xl">FOLLOW US</h1>
-          <a href="https://www.facebook.com/carljohn2818"><span class="text_wrap"><i class="fa-brands fa-facebook pr-2"></i>Facebook</span></a>
-            <span class="text_wrap"><i class="fa-brands fa-twitter pr-2"></i>Twitter</span>
-         <a href="https://www.instagram.com/carljohn_16"><span class="text_wrap"><i class="fa-brands fa-instagram pr-2"></i>Instagram</span></a>
+          <a href="https://www.facebook.com/carljohn2818" target="_blank"><span class="text_wrap"><i class="fa-brands fa-facebook pr-2"></i>Facebook</span></a>
+          <a href="https://www.instagram.com/carljohn_16" target="_blank"><span class="text_wrap"><i class="fa-brands fa-instagram pr-2"></i>Instagram</span></a>
             <a href="https://github.com/CodeWithRibo" target="_blank"><span class="text_wrap"><i class="fa-brands fa-github pr-2"></i>Github</span></a>
           </div>
         </div>
@@ -61,5 +67,47 @@
       </main>
     </section>
     </body>
- </html>
- 
+ </html>';
+ }
+ else {
+  echo '<body>
+  <section class="bg-[#e9e5e5]">
+     <main class="">
+       <div class="main_container mx-auto min-h-full items-start justify-evenly py-20 text-center md:grid md:grid-cols-3 md:gap-10 md:px-20 md:text-start xl:flex xl:gap-44">
+         <div class="m-0 mb-5 flex w-full flex-col gap-2 md:col-start-1 md:m-0 md:gap-5">
+           <h1 class="animation_wrap font-semibold text-primary md:text-xl lg:text-2xl xl:text-[26px] 2xl:text-3xl">QUICK LINKS</h1>
+           <a href="#home"><span class="text_wrap">Home</span></a>
+           <a href="../include/buy_ticket.php #terminals"><span class="text_wrap">Terminals</span></a>
+           <a href="../include/buy_ticket.php"><span class="text_wrap">Buy Ticket</span></a>
+           <a href="../include/home_page.php #about"><span class="text_wrap">About</span></a>
+         </div>
+         <div class="m-0 mb-5 flex w-full flex-col gap-2 md:col-start-1 md:row-start-2 md:m-0 md:gap-5">
+           <h1 class="animation_wrap font-semibold text-primary md:text-xl lg:text-2xl xl:text-[26px] 2xl:text-3xl">CONTACT INFO</h1>
+           <span class="text_wrap">+63 917 987 6543</span>
+           <span class="text_wrap">(02) 8123 4567</span>
+           <span class="text_wrap">JeepneyTicketingSystem@gmail.com</span>
+           <span class="text_wrap">STI Academic Center, 730 Samson Rd Caloocan, Metro Manila</span>
+         </div>
+         <div class="m-0 flex w-full flex-col gap-2 md:col-start-2 md:row-start-1 md:m-0 md:ml-10 md:gap-5">
+           <h1 class="animation_wrap font-semibold text-primary md:text-xl lg:text-2xl xl:text-[26px] 2xl:text-3xl">FOLLOW US</h1>
+         <a href="https://www.facebook.com/carljohn2818" target="_blank"><span class="text_wrap"><i class="fa-brands fa-facebook pr-2"></i>Facebook</span></a>
+         <a href="https://www.instagram.com/carljohn_16" target="_blank"><span class="text_wrap"><i class="fa-brands fa-instagram pr-2"></i>Instagram</span></a>
+           <a href="https://github.com/CodeWithRibo" target="_blank"><span class="text_wrap"><i class="fa-brands fa-github pr-2"></i>Github</span></a>
+         </div>
+       </div>
+       <!-- ^All Rights reserved -->
+       <div>
+         <div class="max-w-full border-b-[1.5px] border-textColor text-center"></div>
+         <div class="py-5 text-center text-base font-bold text-textColor md:text-[17px] xl:text-[19px]">
+           <span>© 2025 JeepneyTicketingSystem™. All Rights Reserved.</span>
+         </div>
+       </div>
+     </main>
+   </section>
+   </body>
+</html>';
+ }
+?>
+
+
+
