5783895d6d5d8f0cb103d9693caf2d1d6b1c2a82
CodeWithRibo <riboluna2818@gmail.com>
2025-02-27 13:49:56 +0800

    fixing error

--- public/js/PriceFare.js
+++ public/js/PriceFare.js
@@ -1,46 +1,56 @@
-const baclaranTerminal = document.getElementById('baclaranTerminal');
-const rectoTerminal = document.getElementById('rectoTerminal');
-const sanjuanTerminal = document.getElementById('sanjuanTerminal');
-const selectDestinations = document.getElementById('selectDestinations');
-const pricefare = document.getElementById('pricefare');
-const farePriceText = document.getElementById('farePriceText'); 
 
-//Baclaran Terminal
-baclaranTerminal.addEventListener('click', (e) =>{
-    e.preventDefault();
-    selectDestinations.textContent = 'Baclaran Terminal';
-    farePriceText.textContent= "Fare Price:"
-    pricefare.textContent = '₱70';
-    pricefare.style.color = 'red';
-    farePriceText.style.color = 'red';
-    pricefare.style.fontWeight = 'bold';
-})
+    //DOM LOADED
+    document.addEventListener('DOMContentLoaded', function () {
 
-//Recto Terminal
-rectoTerminal.addEventListener('click', (e) => {
-    e.preventDefault();
-    selectDestinations.textContent = 'Recto Terminal';
-    farePriceText.textContent= "Fare Price:"
-    pricefare.textContent = '₱40';
-    pricefare.style.color = 'red';
-    farePriceText.style.color = 'red';
-    pricefare.style.fontWeight = 'bold';
-})
+        const baclaranTerminal = document.getElementById('baclaranTerminal');
+        const rectoTerminal = document.getElementById('rectoTerminal');
+        const sanjuanTerminal = document.getElementById('sanjuanTerminal');
+        const selectDestinations = document.getElementById('selectDestinations');
+        const pricefare = document.getElementById('pricefare');
+        const farePriceText = document.getElementById('farePriceText'); 
+        
+        
+        //Store price fare
+        const farePrice = {baclaranTerminal : 70, rectoTerminal : 40, sanjuanTerminal : 55};
+        
+        
+        //Baclaran Terminal
+        baclaranTerminal.addEventListener('click', (e) =>{
+            e.preventDefault();
+            selectDestinations.textContent = 'Baclaran Terminal';
+            farePriceText.textContent= "Fare Price:"
+            pricefare.textContent.style.color = farePrice.baclaranTerminal,'red';
+            farePriceText.style.color = 'red';
+            pricefare.style.fontWeight = 'bold';
+        })
+        
+        // Recto Terminal
+        rectoTerminal.addEventListener('click', (e) => {
+            e.preventDefault();
+            selectDestinations.textContent = 'Recto Terminal';
+            farePriceText.textContent= "Fare Price:"
+            pricefare.textContent = farePrice.rectoTerminal;
+            pricefare.style.color = 'red';
+            farePriceText.style.color = 'red';
+            pricefare.style.fontWeight = 'bold';
+        })
+        
+        //SanJuan Terminal
+        sanjuanTerminal.addEventListener('click', (e) => {
+            e.preventDefault();
+            selectDestinations.textContent = 'SanJuan GreenHills Terminal';
+            farePriceText.textContent= "Fare Price:"
+            pricefare.textContent = farePrice.sanjuanTerminal;
+            pricefare.style.color = 'red';
+            farePriceText.style.color = 'red';
+            pricefare.style.fontWeight = 'bold';
+        })
+        
+        //CONVERTING SPAN TO INPUT
+        
+        document.getElementById('ticketForm').addEventListener('submit', function(){
+             document.getElementById('inputSelectDestinations').value = selectDestinations.textContent;
+             document.getElementById('farePrice').value = pricefare.textContent;
+        });
 
-//Recto Terminal
-sanjuanTerminal.addEventListener('click', (e) => {
-    e.preventDefault();
-    selectDestinations.textContent = 'SanJuan GreenHills Terminal';
-    farePriceText.textContent= "Fare Price:"
-    pricefare.textContent = '₱55';
-    pricefare.style.color = 'red';
-    farePriceText.style.color = 'red';
-    pricefare.style.fontWeight = 'bold';
-})
-
-//CONVERTING SPAN TO INPUT
-
-document.getElementById('ticketForm').addEventListener('submit', function(){
-     document.getElementById('inputSelectDestinations').value = selectDestinations.textContent;
-     document.getElementById('farePrice').value = pricefare.textContent;
-});
+    });
