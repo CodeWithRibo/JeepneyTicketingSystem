2d9a94d333378296de1b027f5eaaf768dba131b6
CodeWithRibo <riboluna2818@gmail.com>
2025-03-02 21:49:02 +0800

    already commited

--- public/Include/transaction_payment.php
+++ public/Include/transaction_payment.php
@@ -1,7 +1,6 @@
 <?php 
 
 include '../handler/transaction_payment_process.php';
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -90,12 +89,20 @@ include '../handler/transaction_payment_process.php';
                 <div class="flex flex-col items-center justify-center pb-10">
                 <h1 class="text-2xl font-semibold text-textColor md:text-3xl">Transaction Payment</h1>
                 </div>
+                <div class="flex pb-10">
+                <a href="buy_ticket.php" id="lugar_terminal" class="under_construction mt-4 flex flex-row-reverse items-center rounded-xl bg-[#d5c812] px-6 py-1 text-lg text-white transition-all duration-300 ease-in hover:bg-primary hover:opacity-85 sm:mt-0 sm:px-8 sm:py-2 sm:text-xl md:px-3 md:text-xl lg:px-5 lg:py-2">
+                Buy Ticket Again
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mr-2 size-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 0 1 0 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 0 1 0-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375Z" />
+                </svg>
+              </a>
+                </div>
                 <div class="pb-18">
                 <table class="table-auto w-full text-center border-collapse ">
                     <tr class="border-gray-400">
                         <th class="py-3  md:text-base lg:text-xl bg-gray-100 text-gray-800">PASSENGER NAME</th>
                         <th class="py-3  md:text-base lg:text-xl bg-blue-100 text-blue-800">ORIGIN & DESTINATION</th>
-                        <th class="py-3  md:text-base lg:text-xl bg-green-100 text-green-800">BOOK DATA & TIME</th>
+                        <th class="py-3  md:text-base lg:text-xl bg-green-100 text-green-800">DATE & TIME</th>
                         <th class="py-3  md:text-base lg:text-xl bg-yellow-100 text-yellow-800">TICKET NUMBER</th>
                         <th class="py-3  md:text-base lg:text-xl bg-emerald-100 text-emerald-500">FARE PRICE</th>
                         <th class="py-3  md:text-base lg:text-xl bg-purple-100 text-purple-800">STATUS</th>
@@ -119,7 +126,7 @@ include '../handler/transaction_payment_process.php';
                       <td class='py-3 flex flex-col justify-center sm:flex-col md:flex-col lg:flex-row'>
                         <form action='transaction_payment.php' method='POST'>
                       <input type='hidden' name='id_to_delete' value='{$row['id']} ?>'>
-                    <button type='submit' name='delete' value='delete' class='md:text-base lg:text-lg py-1 px-2 md:px-3 lg:px-5 bg-red-500 hover:bg-red-800 rounded-lg cursor-pointer hover:opacity-80 transition duration-300 text-white text-lg'>Delete</button>
+                    <button type='submit' name='delete' value='delete' class='md:text-base lg:text-lg py-1 px-2 md:px-3 lg:px-5 bg-red-500 hover:bg-red-800 rounded-lg cursor-pointer hover:opacity-80 transition duration-300 text-white text-lg mr-2'>Delete</button>
                     </form>
                         <button class='md:text-base lg:text-lg py-1 md:px-3 lg:px-4 bg-green-500 hover:bg-green-800 rounded-lg cursor-pointer hover:opacity-80 transition duration-300 text-white text-lg mr-2'>Pay Now</button>
                         </td>
