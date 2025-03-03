9fcbf2463393cf3c208c3b5aa87134e93aee7517
CodeWithRibo <riboluna2818@gmail.com>
2025-03-03 19:30:39 +0800

    formatting the code

--- public/components/footer.php
+++ public/components/footer.php
@@ -3,35 +3,35 @@
 $isLogin = isset($_SESSION['user_id']);
 
 ?>
- <!DOCTYPE html>
- <html lang="en">
- <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="/jts/src/output.css" />
-    <link rel="stylesheet" href="/jts/src/input.css" />
-    <link rel="stylesheet" href="/jts/src/plugin.css" />
-    <link rel="stylesheet" href="/jts/custom.css">
-    <script src="/jts/public/js/index.js"></script>
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
-      integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    />
+<!DOCTYPE html>
+<html lang="en">
 
-    <style>
-      @media (max-width: 550px) {
-        .responsive_img {
-          height: auto;
-        }
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="/jts/src/output.css" />
+  <link rel="stylesheet" href="/jts/src/input.css" />
+  <link rel="stylesheet" href="/jts/src/plugin.css" />
+  <link rel="stylesheet" href="/jts/custom.css">
+  <script src="/jts/public/js/index.js"></script>
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+    integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+    crossorigin="anonymous"
+    referrerpolicy="no-referrer" />
+
+  <style>
+    @media (max-width: 550px) {
+      .responsive_img {
+        height: auto;
       }
-    </style>
- </head>
+    }
+  </style>
+</head>
 <?php
- if (!$isLogin) {
+if (!$isLogin) {
   echo '<body>
    <section class="bg-[#e9e5e5]">
       <main class="">
@@ -68,8 +68,7 @@ $isLogin = isset($_SESSION['user_id']);
     </section>
     </body>
  </html>';
- }
- else {
+} else {
   echo '<body>
   <section class="bg-[#e9e5e5]">
      <main class="">
@@ -106,8 +105,5 @@ $isLogin = isset($_SESSION['user_id']);
    </section>
    </body>
 </html>';
- }
+}
 ?>
-
-
-
