e0871572c01fbd82abcef3cd3a41e382f9a27dc4
CodeWithRibo <riboluna2818@gmail.com>
2025-03-03 19:32:39 +0800

    formatting the code

--- public/Login/login_process.php
+++ public/Login/login_process.php
@@ -1,73 +1,75 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
+
 <body>
-<?php 
-include '../Database/dbconfig.php';
+    <?php
+    include '../Database/dbconfig.php';
 
-session_start();
+    session_start();
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $userName = $_POST["userName"];
-    $password = $_POST["password"];
+        $userName = $_POST["userName"];
+        $password = $_POST["password"];
 
-    // check if username and password are  empty
-    if (empty($userName) || empty($password)) {
-        ?>
-        <script>
-            Swal.fire({
+        // check if username and password are  empty
+        if (empty($userName) || empty($password)) {
+    ?>
+            <script>
+                Swal.fire({
                     title: "Login Failed",
                     text: "Please enter both username and password",
                     icon: "error"
-            }).then((result) => {
-                        if (result.isConfirmed) {
-                            window.location = 'login.php';
-                        }
-                    });
-        </script>
-        <?php 
-        
-        exit;
-    }
-    // retrieve data in the database base on the username and password
-    $sqlUsername = "SELECT id, userName, password FROM jts_users WHERE userName = ?";
-    $checkUserNameStmt = $connection -> prepare($sqlUsername);
-    $checkUserNameStmt -> bind_param("s", $userName);
-    $checkUserNameStmt -> execute();
-    $result = $checkUserNameStmt -> get_result();
-    // get the result by single row and store it in $user
-    $user = $result -> fetch_assoc();
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.location = 'login.php';
+                    }
+                });
+            </script>
+        <?php
 
-    // verify the password
-    if($user && $password === $user['password']) {
-       // store the user id in the session
-        $_SESSION["user_id"] = $user['id'];
-        $_SESSION["user_name"] = $user['userName'];
-    } 
-    else {
-        // authentication failed
+            exit;
+        }
+        // retrieve data in the database base on the username and password
+        $sqlUsername = "SELECT id, userName, password FROM jts_users WHERE userName = ?";
+        $checkUserNameStmt = $connection->prepare($sqlUsername);
+        $checkUserNameStmt->bind_param("s", $userName);
+        $checkUserNameStmt->execute();
+        $result = $checkUserNameStmt->get_result();
+        // get the result by single row and store it in $user
+        $user = $result->fetch_assoc();
+
+        // verify the password
+        if ($user && $password === $user['password']) {
+            // store the user id in the session
+            $_SESSION["user_id"] = $user['id'];
+            $_SESSION["user_name"] = $user['userName'];
+        } else {
+            // authentication failed
         ?>
-        <script>
-            Swal.fire({
+            <script>
+                Swal.fire({
                     title: "Login Failed",
                     text: "Invalid username or password",
                     icon: "error"
-            }).then((result) => {
-                        if (result.isConfirmed) {
-                            window.location = 'login.php';
-                        }
-                    });
-                    </script>
-     <?php 
-        exit;
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.location = 'login.php';
+                    }
+                });
+            </script>
+    <?php
+            exit;
+        }
     }
-}
-$connection -> close();
-?>
+    $connection->close();
+    ?>
 
 </body>
+
 </html>
