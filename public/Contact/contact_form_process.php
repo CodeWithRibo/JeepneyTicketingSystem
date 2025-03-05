67fe23affca2f36c32e0f45a4299148db218ba7e
CodeWithRibo <riboluna2818@gmail.com>
2025-03-05 13:56:21 +0800

    adding foreign key to store the data

--- public/Contact/contact_form_process.php
+++ public/Contact/contact_form_process.php
@@ -1,44 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <title>Document</title>
 </head>
-<body>
-<?php 
-include '../Database/dbconfig.php';
-if($_SERVER["REQUEST_METHOD"] == "POST") {
- 
-    include '../handler/contact_logic_handler.php';
-    ob_start();
-   // Validation
-    if(!array_filter($contactErrors))  {
-         // if no errors proceed to the db
-         $sqlContact = "INSERT INTO contact (firstName, lastName, email, subject, message) VALUES (?,?,?,?,?)";
-         $insertContact = $connection -> prepare($sqlContact);
-         $insertContact -> bind_param("sssss", $firstName,$lastName, $email,$subject,$message);              
-         if($insertContact -> execute()) {
-            ?> <script> 
-            Swal.fire({
-                position: "top-end",
-                icon: "success",
-                title: "Your message has been submitted",
-                showConfirmButton: false,
-                timer: 1500
-                }).then(() => {
-                    window.location = '../include/home_page.php';
-                });
-            </script> <?php
-             exit();
-         } 
-         $insertContact -> close();
-         $connection -> close();
-    }
-    ob_end_flush();
-}
-?>
-</body>
-</html>
 
+<body>
+    <?php
+    include '../Database/dbconfig.php';
+
+        if(!isset($_SESSION['user_id'])) {
+            header ('Location: ../Logout/home_page.php');
+            exit();
+        }
+
+        $userId = $_SESSION['user_id'];
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        include '../handler/contact_logic_handler.php';
+        ob_start();
+        // Validation
+        if (!array_filter($contactErrors)) {
+            // if no errors proceed to the db
+            $sqlContact = "INSERT INTO contact (firstName, lastName, email, subject, message, user_id) VALUES (?, ? , ? , ? , ? , ? )";
+            $insertContact = $connection->prepare($sqlContact);
+            $insertContact->bind_param("sssssi", $firstName, $lastName, $email, $subject, $message, $userId);
+            if ($insertContact->execute()) {
+    ?> <script>
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "Your message has been submitted",
+                        showConfirmButton: false,
+                        timer: 1500
+                    }).then(() => {
+                        window.location = '../include/home_page.php';
+                    });
+                </script> <?php
+                            exit();
+                        }
+                        $insertContact->close();
+                        $connection->close();
+                    }
+                    ob_end_flush();
+                }
+                            ?>
+</body>
+
+</html>
