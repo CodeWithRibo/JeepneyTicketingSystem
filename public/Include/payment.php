51a42dda84dac1ddf2584b8d2bcc2394ce414d66
CodeWithRibo <riboluna2818@gmail.com>
2025-03-05 15:20:49 +0800

    adding passenger details

---
+++ public/Include/payment.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Payment</title>
+</head>
+
+<body>
+    <section class="main_container">
+        <div class="flex flex-row justify-center items-center">
+            <div>
+                <h1>PASSENGER DETAILS</h1>
+            </div>
+            <!-- INFORMATIONS -->
+            <div class="">
+                <div class="">
+                    <span class="">Passenger's Name: CarlJohn Sto Tomas</span>
+                    <span>Passenger's Email: carljohnstotomas@gmail.com</span>
+                    <span>Passenger's PhoneNumber: 093340417</span>
+                    <span>Passenger's Origin: Monumento Terminal</span>
+                    <span>Passenger's Destinations: Baclaran Terminal</span>
+                    <span>Book Date and Time: March 5, 2025</span>
+                    <span>No. Passengers: 3</span>
+                    <span>No. of Passengers with Discount: 4 </span>
+                    <span>Fare Price: 70</span>
+                </div>
+                <div>
+                    <span>Total No. Passenger: </span>
+                    <span>Total No. of Passenger with Discount: </span>
+                    <span>Total Fare Price: 100</span>
+                </div>
+            </div>
+            <div>
+                    <h2>PWD, STUDENT, SENIOR CITIZEN DISCOUNT 20%</h2>
+                </div>
+        </div>
+    </section>
+</body>
+
+</html>
