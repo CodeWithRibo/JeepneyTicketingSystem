eda9b4bebb2344c7ee241375ee7a1892508dce5d
CodeWithRibo <riboluna2818@gmail.com>
2025-03-07 18:08:21 +0800

    Paymongo API Payment method

---
+++ public/handler/payment_controller.php
@@ -0,0 +1,51 @@
+<?php
+
+include '../Database/dbconfig.php';
+session_start();
+
+if($_SERVER["REQUEST_METHOD"] == 'POST') {
+    // PAYMONGO API
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+    CURLOPT_URL => "https://api.paymongo.com/v1/links", // API KEY for Paymongo
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS => json_encode([
+        'data' => [
+            'attributes' => [
+                'amount' => $_SESSION['convertTotalFarePrice'],
+                'description' => "Payment for " . $_SESSION['passengerFirstName'] . ' ' . $_SESSION['passengerLastName'],
+                'remarks' => "TESTING"
+            ]
+        ]
+    ]),
+    CURLOPT_HTTPHEADER => [
+        "accept: application/json",
+        "authorization: Basic c2tfdGVzdF9CU3RRYWtSZG5Fb255RHdjcWNrREYyNXk6",
+        "content-type: application/json"
+    ],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+    echo json_encode(['Error' => "cURL Error #;" . $err]);
+} else {
+    $responseData = json_decode($response, true);
+    if (isset($responseData['data']['attributes']['checkout_url'])) {
+        $checkout_url = $responseData['data']['attributes']['checkout_url'];
+        echo "<script>window.location.href = '$checkout_url';</script>";
+    } else {
+        echo json_encode(['Error' => "No URL found"]);
+    }
+}
+
+}
