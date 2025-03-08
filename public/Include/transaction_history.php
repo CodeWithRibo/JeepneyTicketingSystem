73c8f51c980dde76ed2ac8cd0c1c9d7d91c601ad
CodeWithRibo <riboluna2818@gmail.com>
2025-03-08 23:16:55 +0800

    new file transaction_history.php

---
+++ public/Include/transaction_history.php
@@ -0,0 +1,47 @@
+<?php
+
+include '../Database/dbconfig.php';
+
+session_start();
+
+$rows = [];
+
+$userId = $_SESSION['user_id'];
+$ticketNumber = $_SESSION['passengerTicketNumber'];
+
+$sql = "SELECT * FROM transaction_history WHERE user_id = ? AND ticketNumber = ?";
+$stmt = $connection->prepare($sql);
+$stmt->bind_param('is', $userId, $ticketNumber);
+$stmt->execute();
+$result = $stmt->get_result();
+
+while ($row = $result->fetch_assoc()) {
+    $rows[] = $row;
+}
+
+?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php 
+    foreach ($rows as $row) {
+        echo $row['firstName'];
+        echo $row['lastName'];
+        echo $row['dateAndTime'];
+        echo $row['optionOrigin'];
+        echo $row['optionDestinations'];
+        echo $row['ticketNumber'];
+        echo $row['farePrice'];
+        echo $row['totalFarePrice'];
+        echo $row['status'];
+    }
+
+?> 
+</body>
+</html>
