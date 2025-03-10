6854313761eef032b815d9d3dd59da28a06eba5a
CodeWithRibo <riboluna2818@gmail.com>
2025-03-10 19:49:28 +0800

    already commited

--- public/handler/payment_controller.php
+++ public/handler/payment_controller.php
@@ -68,6 +68,7 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
             $stmtDeleteTransaction -> execute();
         } else {
             echo json_encode(['Error' => "No URL found"]);
+            
         }
     }
 }
