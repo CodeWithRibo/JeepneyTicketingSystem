513f895f08f057b9c41c1030ce4db0c40e60506e
CodeWithRibo <riboluna2818@gmail.com>
2025-03-11 18:17:17 +0800

    adding logic for changing email

---
+++ public/handler/settings_process.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Settings</title>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</head>
+
+<body>
+    <?php
+    include '../Database/dbconfig.php';
+
+    if (isset($_SESSION['user_id'])) {
+        $isLogin = $_SESSION['user_id'];
+    }
+    //HANDLING ERROR
+    $validateEmail = ['email' => ""];
+
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        $newEmail = $_POST['newEmail'];
+
+        if (empty($newEmail)) {
+            $validateEmail['email'] = "Please enter your Email";
+        } else if (!filter_var($newEmail, FILTER_VALIDATE_EMAIL)) {
+            $validateEmail['email'] = "Invalid email format";
+        }
+
+        if (!array_filter($validateEmail)) {
+            $sql = "UPDATE jts_users SET email = ? WHERE id = ?";
+            $stmt = $connection->prepare($sql);
+            $stmt->bind_param('si', $newEmail, $isLogin);
+
+            if ($stmt->execute()) {
+    ?>
+                <script>
+                    Swal.fire({
+                        title: "Successful Change Email Address!",
+                        text: "Your email address has been successfully updated",
+                        icon: "success"
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            window.location.href = "../include/settings.php";
+                        }
+                    });
+                </script>
+    <?php
+            }
+            $connection->close();
+            $stmt->close();
+        }
+    } else {
+        echo 'POST REQUEST DENIED';
+    }
+    ?>
+</body>
+
+</html>
