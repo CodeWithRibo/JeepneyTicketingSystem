01f438c05936df968237c34afdd6e4165e6ce279
CodeWithRibo <riboluna2818@gmail.com>
2025-03-11 20:04:09 +0800

    adding logic for new and confirm password

--- public/handler/settings_process.php
+++ public/handler/settings_process.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Settings</title>
+    <title>Settings change email</title>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 
@@ -15,43 +15,87 @@
     if (isset($_SESSION['user_id'])) {
         $isLogin = $_SESSION['user_id'];
     }
-    //HANDLING ERROR
-    $validateEmail = ['email' => ""];
 
     if ($_SERVER['REQUEST_METHOD'] == "POST") {
-        $newEmail = $_POST['newEmail'];
+        if (isset($_POST['changeEmail'])) {
+            $newEmail = $_POST['newEmail'];
+            $currentPassword = $_POST['currentPassword'];
 
-        if (empty($newEmail)) {
-            $validateEmail['email'] = "Please enter your Email";
-        } else if (!filter_var($newEmail, FILTER_VALIDATE_EMAIL)) {
-            $validateEmail['email'] = "Invalid email format";
-        }
+            include 'settings_logic_handler.php';
 
-        if (!array_filter($validateEmail)) {
-            $sql = "UPDATE jts_users SET email = ? WHERE id = ?";
-            $stmt = $connection->prepare($sql);
-            $stmt->bind_param('si', $newEmail, $isLogin);
+            $passwordSQL = "SELECT password FROM jts_users WHERE id = ?";
+            $passwordSTMT = $connection->prepare($passwordSQL);
+            $passwordSTMT->bind_param('i', $isLogin);
+            $passwordSTMT->execute();
+            $result = $passwordSTMT->get_result();
+            $userPassword = $result->fetch_assoc();
 
-            if ($stmt->execute()) {
+            if ($currentPassword === $userPassword['password']) {
+                $sql = "UPDATE jts_users SET email = ? WHERE id = ?";
+                $stmt = $connection->prepare($sql);
+                $stmt->bind_param('si', $newEmail, $isLogin);
+
+                if ($stmt->execute()) {
     ?>
-                <script>
-                    Swal.fire({
-                        title: "Successful Change Email Address!",
-                        text: "Your email address has been successfully updated",
-                        icon: "success"
-                    }).then((result) => {
-                        if (result.isConfirmed) {
-                            window.location.href = "../include/settings.php";
-                        }
-                    });
-                </script>
+                    <script>
+                        Swal.fire({
+                            title: "Successful Change Email Address!",
+                            text: "Your email address has been successfully updated",
+                            icon: "success"
+                        }).then((result) => {
+                            if (result.isConfirmed) {
+                                window.location.href = "../include/settings.php";
+                            }
+                        });
+                    </script>
     <?php
+                }
+                $stmt->close();
+            } else {
+                echo 'Current password is incorrect';
             }
-            $connection->close();
-            $stmt->close();
+            $passwordSTMT->close();
+        } elseif (isset($_POST['changePassword'])) {
+            $currentPassword = $_POST['currentPassword'];
+            $newPassword = $_POST['newPassword'];
+            $confirmPassword = $_POST['confirmPassword'];
+
+            include 'settings_logic_handler.php';
+
+            $passwordSQL = "SELECT password FROM jts_users WHERE id = ?";
+            $passwordSTMT = $connection->prepare($passwordSQL);
+            $passwordSTMT->bind_param('i', $isLogin);
+            $passwordSTMT->execute();
+            $result = $passwordSTMT->get_result();
+            $userPassword = $result->fetch_assoc();
+
+            if ($currentPassword === $userPassword['password']) {
+                if ($newPassword === $confirmPassword){
+                    $sql = "UPDATE jts_users SET password = ?, confirmPassword = ? WHERE id = ?";
+                    $stmt = $connection->prepare($sql);
+                    $stmt->bind_param('ssi', $newPassword, $confirmPassword, $isLogin);
+
+                    if ($stmt->execute()) {
+    ?>
+                        <script>
+                            Swal.fire({
+                                title: "Successful Change Password!",
+                                text: "Your password has been successfully updated",
+                                icon: "success"
+                            }).then((result) => {
+                                if (result.isConfirmed) {
+                                    window.location.href = "../include/settings.php";
+                                }
+                            });
+                        </script>
+    <?php
+                    }
+                    $stmt->close();
+                }
+            }
+            $passwordSTMT->close();
         }
-    } else {
-        echo 'POST REQUEST DENIED';
+        $connection->close();
     }
     ?>
 </body>
