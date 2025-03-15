179c7c6db140a03a1db0260fb4d3f32be495e6bf
CodeWithRibo <riboluna2818@gmail.com>
2025-03-15 22:14:48 +0800

    Linking issuedTicket.php

--- public/Conductor/conductor_dashboard.php
+++ public/Conductor/conductor_dashboard.php
@@ -42,7 +42,7 @@
                 <i class="fa-solid fa-table mr-3"></i>
                 Dashboard
             </a>
-            <a href="IssueTicket.html" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+            <a href="conductor_issueTicket.php" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
                 <i class="fas fa-sticky-note mr-3"></i>
                 Issue Ticket
             </a>
@@ -102,7 +102,7 @@
                     <i class="fas fa-sticky-note mr-3"></i>
                     Issue Ticket
                 </a>
-                <a href="tables.html" class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
+                <a href="conductor_issueTicket.php" class="flex items-center text-white opacity-75 hover:opacity-100 py-2 pl-4 nav-item">
                     <i class="fas fa-table mr-3"></i>
                     View Issued Ticket
                 </a>
